e2f158355f1018b378e5230f649596cdb0d8229b
Jinfeng Huang <jinfeng@JinfengdeMacBook-Pro.local>
2015-03-21 14:38:13 -0700

    Initial Commit

---
+++ Music2Tests/Music2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  Music2Tests.swift
+//  Music2Tests
+//
+//  Created by Jinfeng Huang on 15/3/21.
+//  Copyright (c) 2015年 Jinfeng Huang. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Music2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
